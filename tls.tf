ab1471b0c70cc741ea5fcc16290222df33f51373
Adam Szynkowski <adam.szynkowski@oracle.com>
2022-02-23 13:50:44 +1100

    shhfix

--- tls.tf
+++ tls.tf
@@ -2,13 +2,12 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 resource "tls_private_key" "ssh_key" {
-  count = var.ssh_authorized_keys != null ? 0 : 1
   algorithm   = "RSA"
 }
 
 locals {
 
-gen_public_key = chomp(tls_private_key.ssh_key[count.index].public_key_openssh)
-gen_priv_key = tls_private_key.ssh_key[count.index].private_key_pem
+gen_public_key = tls_private_key.ssh_key.public_key_openssh)
+gen_priv_key = tls_private_key.ssh_key.private_key_pem
 
 }
