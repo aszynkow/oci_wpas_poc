63ab0ff3cd9ca756189e15d684765c68579d29d6
Adam Szynkowski <adam.szynkowski@oracle.com>
2022-02-24 09:30:48 +1100

    depnedson

--- volumes.tf
+++ volumes.tf
@@ -25,8 +25,34 @@ resource oci_core_volume ocfs2_shared_10gb {
   vpus_per_gb = "10"
 }
 
+resource oci_core_volume_attachment mastervolatt1 {
+  attachment_type = "paravirtualized"
+  device          = "/dev/oracleoci/oraclevdb"
+  display_name    =  join("",["volumeattachmenocfs2",var.master_vm - 1])
+  #encryption_in_transit_type = <<Optional value not found in discovery>>
+  instance_id                         = oci_core_instance.APP162[var.master_vm - 1].id
+  is_pv_encryption_in_transit_enabled = "false"
+  is_read_only                        = "false"
+  is_shareable = "true"
+  #use_chap = <<Optional value not found in discovery>>
+  volume_id = oci_core_volume.ocfs2_shared.id
+}
+
+resource oci_core_volume_attachment mastervolatt2 {
+  attachment_type = "paravirtualized"
+  device          = "/dev/oracleoci/oraclevdc"
+  display_name    = join("",["volumeattachmenocfs210gb",var.master_vm - 1])
+  #encryption_in_transit_type = <<Optional value not found in discovery>>
+  instance_id                         = oci_core_instance.APP162[var.master_vm - 1].id
+  is_pv_encryption_in_transit_enabled = "false"
+  is_read_only                        = "false"
+  is_shareable = "true"
+  #use_chap = <<Optional value not found in discovery>>
+  volume_id = oci_core_volume.ocfs2_shared_10gb.id
+}
+
 resource oci_core_volume_attachment volumeattachment20220125063516 {
-  count = local.vm_count
+  count = local.vm_count - 1
   attachment_type = "paravirtualized"
   device          = "/dev/oracleoci/oraclevdb"
   display_name    =  join("",["volumeattachmenocfs2",count.index])
@@ -40,7 +66,7 @@ resource oci_core_volume_attachment volumeattachment20220125063516 {
 }
 
 resource oci_core_volume_attachment volumeattachment20220207044931 {
-  count = local.vm_count
+  count = local.vm_count - 1 
   attachment_type = "paravirtualized"
   device          = "/dev/oracleoci/oraclevdc"
   display_name    = join("",["volumeattachmenocfs210gb",count.index])
